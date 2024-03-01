5fa1b842fdfed06a3d4ab21d74d1bfc0fecd0957
Sahil Mukesh Ambre <59484827+sahilambre@users.noreply.github.com>
2024-02-29 22:36:26 -0500

    Logic Building

--- Full Stack/Frontend/Javascript/hello.js
+++ Full Stack/Frontend/Javascript/hello.js
@@ -12,3 +12,23 @@ function crazyString(something, someone, somewhere) {
   return `${someone} was looking for ${something} in general vicinity of ${somewhere}`;
 }
 console.log(crazyString("book", "sahil", "classroom"));
+
+// ! Javascript Arrow functions
+
+function areaCircle1(radius) {
+  let pi = 3.14;
+  return pi * radius * radius;
+}
+
+console.log(areaCircle1(4));
+
+let areaCircle2 = (_) => {
+  const pi = 3.14;
+  return pi * radius * radius;
+};
+
+console.log(areaCircle2(5));
+
+let areaCircle3 = (radius) => 3.14 * radius * radius;
+
+console.log(areaCircle3(8));
---
+++ Logic Building/list.py
@@ -0,0 +1,18 @@
+# Given a list, write a Python program to swap first and last element of the list.
+# Input : [12, 35, 9, 56, 24]
+# Output : [24, 35, 9, 56, 12]
+
+# Input : [1, 2, 3]
+# Output : [3, 2, 1]
+
+def swapList(newList):
+    size = len(newList)
+    temp = newList[0]
+    newList[0] = newList[size-1]
+    newList[size-1] = temp
+
+    return newList
+
+newList = [24, 35, 9, 56, 12]
+print(swapList(newList))
+    
